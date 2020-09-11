47e2cc750ebaa2c2d67a9a6889bbf2f433970aa7
IzzyCH <sunnych741@gmail.com>
2020-09-12 00:08:09 +0800

    Update push.js

--- push.js
+++ push.js
@@ -8,13 +8,15 @@ let vapidKeys = {
 push.setVapidDetails('izzych:sunnych741@1utar.my', vapidKeys.publicKey, vapidKeys.privateKey)
 
 let sub ={
-    endpoint:"https://fcm.googleapis.com/fcm/send/c0QsgJCCdag:APA91bHCfiRfLuOkKUIDEzk8YbuthrOL5B8herEPRpayeWlMjGxtIZZx2baRIcq65UL19BN-41VKw3nvpOnVp3R185dYTD3YDAX_HrAxFfjn-KoQ5mfDHqwE3G8tA1XDfeirXAlUiwtl",
+    "endpoint":"https://fcm.googleapis.com/fcm/send/eHp-V9Lz9SI:APA91bEIgo9i8iuB412D_LuYX55Kb9QLBHm3up_q5GEa0BT5or3rWHGD8py8eRTvm06mtB9IUCEpSn4Dw4_z_m8un4gefu4dvVXrLotEV6-euyOdE33p3UYJsoVwgIGw7_a-5HxBAfOR",
     expirationTime:null,
     keys:{
         p256dh:
-        "BEofckH-3NTioAfdaaA9u3VBb0SAfFYX4U3HMt7qyQjIat95jyNmHdPi-Gya2Sx4GTCX_kEMQ457V3P8jnx67Vs",
-        auth:"83X_7PvPRP_iUCwpKMkc1Q"
+        "BNrkEIqWW4bppLP7IjWLF6kZTfHcrmqW0nvsy5z6ZDcGFO5EWIlIN0eO4H_6VMNe0bwefgGXsq5QlJ7vhZNAoO4",
+        auth:"xt5AqeTcTUiwM4xOUX5wbw"
     }
 };
+
+
 
 push.sendNotification(sub, 'test message')
